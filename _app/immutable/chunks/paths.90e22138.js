0f2c45166e9fcdd55a60ebca520fc11983ae3b6c
Svyatoslav Pushkarev <push2001sla@gmail.com>
2023-11-15 14:56:38 +0500

    deploy

---
+++ _app/immutable/chunks/paths.90e22138.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_13um8ye)==null?void 0:s.base)??"/pushsla.github.io";var e;const t=((e=globalThis.__sveltekit_13um8ye)==null?void 0:e.assets)??a;export{t as a,a as b};
