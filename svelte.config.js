8b97bcacc8acce585002fb7338cea93f73da4217
Svyatoslav Pushkarev <push2001sla@gmail.com>
2023-11-15 13:27:06 +0500

    project setup

---
+++ svelte.config.js
@@ -0,0 +1,33 @@
+import adapter from '@sveltejs/adapter-static';
+import sveltePreprocess from "svelte-preprocess";
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte'],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [
+		sveltePreprocess({
+			markupTagName: "template",
+			postcss: true,
+			sourceMap: true
+		}),
+		vitePreprocess()
+	],
+
+	vitePlugin: {
+		inspector: true,
+	},
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+		paths: {
+			base: process.env.NODE_ENV === "production" ? "/pushsla.github.io" : ""
+		}
+	}
+};
+export default config;
